11b84f11653b94cd9507ba7d70478c5525d1d82f
Bettina Staartjes <bbstaars@gmail.com>
2022-09-01 16:18:23 -0400

    fixed Highscores and Reload buttons at the end of the quiz

--- highscores.js
+++ highscores.js
@@ -1,10 +1,27 @@
+var highScore = document.querySelector("#highScore");
+var clear = document.querySelector("#clear");
+var goBack = document.querySelector("#goBack");
 
+// Event listener to clear scores 
+clear.addEventListener("click", function () {
+    localStorage.clear();
+    location.reload();
+});
+// Retreives local stroage 
+var allScores = localStorage.getItem("allScores");
+allScores = JSON.parse(allScores);
 
+if (allScores !== null) {
 
+    for (var i = 0; i < allScores.length; i++) {
 
-submitBtn.addEventListener('click', () => {
-        quiz.innerHTML = `
-        <button onclick = "location.reload()">Go Back</button>
-        <button onclick = "location.reload()">Reload</button>
-        `
-    })
+        var createLi = document.createElement("li");
+        createLi.textContent = allScores[i].initials + " " + allScores[i].score;
+        highScore.appendChild(createLi);
+
+    }
+}
+// Event listener to move to index page
+goBack.addEventListener("click", function () {
+    window.location.replace("./index.html");
+});
--- questions.js
+++ questions.js
@@ -3,7 +3,7 @@
 //Setting up the timer
 var timer = 60;
 var interval = setInterval (function() {
-    document.getElementById('timer').innerHTML = timer;
+    document.getElementById('timer').innerHTML = "Time Left: "+timer+" s";
     timer --;
     if (timer === 0) {
         clearInterval(interval);
@@ -59,7 +59,7 @@ const quizData = [
 
 ];
 
-//Variables...?
+//Variables (const (things that cannot change)=var/let (re-assignable), let is better than var
 const quiz= document.getElementById('quiz')
 const answerEls= document.querySelectorAll('.answer')
 const questionEl = document.getElementById('question')
@@ -67,7 +67,11 @@ const a_text = document.getElementById('a_text')
 const b_text = document.getElementById('b_text')
 const c_text = document.getElementById('c_text')
 const d_text = document.getElementById('d_text')
-const submitBtn = document.getElementById ('submit')
+const submitBtn = document.getElementById('submit')
+//have to do camel case, cant have two submitBtns
+const submitInitials = document.getElementById ('submit-initials')
+//CORRECT DISPLAY
+const displayCorrect = document.getElementById ('display-correct')
 
 let currentQuiz = 0
 let score = 0
@@ -78,7 +82,7 @@ function loadQuiz() {
     deselectAnswers()
 
     const currentQuizData = quizData[currentQuiz]
-
+//LOOK HOW WE CAN LOOP/CONDENSE THIS
     questionEl.innerText = currentQuizData.question
     a_text.innerText = currentQuizData.a
     b_text.innerText = currentQuizData.b
@@ -100,25 +104,100 @@ function getSelected() {
     return answer
 }
 
+var saveInitials = function() {
+    localStorage.setItem("tasks", tasks);
+}
+
+//GETTING CORRECT ANSWER AND INCORRECT ANSWER
 submitBtn.addEventListener('click', () => {
     const answer = getSelected()
     if(answer) {
         if(answer === quizData[currentQuiz].correct) {
             score++
+            displayCorrect.innerHTML = "Correct"
+            //alert ("Correct")
+        // - = deducting an amount from a variable    
+        } else {
+            timer-=10
+            displayCorrect.innerHTML = "Incorrect"
         }
     currentQuiz ++
 
     if(currentQuiz < quizData.length) {
         loadQuiz()
     } else {
+        //Replacing below with HTML
         quiz.innerHTML = `
         <br><h1> All Done! </h1></br>
         <h2> You answered ${score}/${quizData.length} questions correctly</h2>
         <h3> Enter your initials:</h3>
-        <br></br>
-        <button onclick = "location.href="./highscores.html";()">View Highscores</button>
-        <button onclick = "location.reload()">Reload</button>
+        <!--FOR ENTERING YOUR INITIALS AT THE END-->
+            <form id = "task-form">
+                <div class="form-group">
+                <input type="text" name="initials" placeholder="Enter Your Initials" />
+                </div>
+                <div class="form group">
+                <button class="btn" id="submit-initials" type="submit">Submit High Score</button>
+                </div>
+            </form>
+        <button onclick = "location.href='highscores.html';">View Highscores</button>
+        <button onclick = "location.reload();">Reload</button>
         `
     }
     }
 })
+
+// Label
+var createLabel = document.createElement("label");
+createLabel.setAttribute("id", "createLabel");
+createLabel.textContent = "Enter your initials: ";
+
+questionsDiv.appendChild(createLabel);
+
+// input
+var createInput = document.createElement("input");
+createInput.setAttribute("type", "text");
+createInput.setAttribute("id", "initials");
+createInput.textContent = "";
+
+questionsDiv.appendChild(createInput);
+
+// submit
+var createSubmit = document.createElement("button");
+createSubmit.setAttribute("type", "submit");
+createSubmit.setAttribute("id", "Submit");
+createSubmit.textContent = "Submit";
+
+questionsDiv.appendChild(createSubmit);
+
+// Event listener to capture initials and local storage for initials and score
+createSubmit.addEventListener("click", function () {
+    var initials = createInput.value;
+
+    if (initials === null) {
+
+        console.log("No value entered!");
+
+    } else {
+        var finalScore = {
+            initials: initials,
+            score: timeRemaining
+        }
+        console.log(finalScore);
+        var allScores = localStorage.getItem("allScores");
+        if (allScores === null) {
+            allScores = [];
+        } else {
+            allScores = JSON.parse(allScores);
+        }
+        allScores.push(finalScore);
+        var newScore = JSON.stringify(allScores);
+        localStorage.setItem("allScores", newScore);
+        // Travels to final page
+        window.location.replace("./HighScores.html");
+    }
+});
+
+//save in local storage to send initials to next high score, saving somethinig permanently in browser; 
+
+//maybe event.preventdefault somwhere????
