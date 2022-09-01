80310c6cb910dde1f390340a2332dfcfd45c5297
Bettina Staartjes <bbstaars@gmail.com>
2022-09-01 17:11:32 -0400

    organized files and folders

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -194,7 +194,7 @@ createSubmit.addEventListener("click", function () {
         var newScore = JSON.stringify(allScores);
         localStorage.setItem("allScores", newScore);
         // Travels to final page
-        window.location.replace("./HighScores.html");
+        window.location.replace("highscores.html");
     }
 });
 
